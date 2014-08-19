4476a6ba67245aa01ded7340a5d8d22250cd87e7
Konstantinos Krestenitis <contact@ikonstantinos.com>
2014-08-19 04:24:18 +0100

    fix2

--- Sequential/main.cpp
+++ Sequential/main.cpp
@@ -5,9 +5,8 @@ int main (){
 	unsigned int kLength = 0, counter=0, digit = 0;
 	//find largest length number;
 	for(unsigned int i=0;i<5;i++){
-		if(number[i]==0) break;
-        digit=number[i];
-		while((digit = digit/10)!=0){ counter++;}
+		if((digit=number[i])==0) break;
+		while((digit = digit/10)!=0){counter++;}
 		if(counter > kLength) kLength = counter;
 		counter=0;
 	}
@@ -15,9 +14,9 @@ int main (){
 	int bucket[10][5] = {0}; bool hasValue[10][5] = {false};
 	for(unsigned int i=0;i<kLength+1;i++){
 		for(unsigned int y=0;y<5;y++){
-            if(!(digit = number[y])/10==0)
-                for(unsigned int k=0;k<i;k++) digit = digit/10; // got to digit index
-			if((digit = digit%10)==0) digit=1; //if digit = 0 the set to 1 to keep in array boundary
+            if(!(digit = number[y])/10==0) //if not last checked digit index
+                for(unsigned int k=0;k<i;k++) digit = digit/10; // go to last digit index
+			if((digit = digit%10)==0) digit=1; //if digit = 0 set to 1 to keep within array boundary
 			printf("number:%i digit:%i\n", number[y], digit);
             for(unsigned int c=0; c<5;c++){
                 if(hasValue[digit-1][c]!=true){
@@ -26,8 +25,7 @@ int main (){
                     break;
                 }
             }
-		}
-		counter=0;
+		}counter=0;
 		for(unsigned int y=0;y<10;y++){
 			for(unsigned int x=0;x<5;x++){
 				if(hasValue[y][x]==true) {
@@ -36,8 +34,7 @@ int main (){
 					hasValue[y][x]=false;
 				}
 			}
-		}
-		for(unsigned int y=0;y<5;y++){printf("Cell[%i]=%i\n", y, number[y]);}
+		}for(unsigned int y=0;y<5;y++){printf("Cell[%i]=%i\n", y, number[y]);}
 	}
-	return 0;
+    return 0;
 }
